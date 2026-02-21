312b707abf8b3841e84d65c2545d6da74ff32c75
Valentyn <ziomenkow@gmail.com>
2026-02-21 03:15:36 +0100

    add functional in form js released textArea input + email input saved to formData

--- src/js/form.js
+++ src/js/form.js
@@ -5,27 +5,39 @@ const formData = {
     message : "",
  }
 
- printTextarea();
+
 
  const form = document.querySelector(".feedback-form");
  const textarea = form.querySelector("textarea");
+ const email = form.querySelector("input");
+ printTextarea();
 
+// on submit
  form.addEventListener("submit", handleSubmit);
 
  function handleSubmit(event) {
   event.preventDefault();
-
   event.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
- }
+  console.log(formData);
 
+ }
+ // on email input 
+ email.addEventListener("input", (event) => {
+  formData.email = event.target.value;
+ })
+
+
+// on textarea input
  textarea.addEventListener("input", textAreaInput)
 
  function textAreaInput(event) {
-   const message = event.target.value;
-   localStorage.setItem(STORAGE_KEY, message)
+   const areamessage = event.target.value;
+   localStorage.setItem(STORAGE_KEY, areamessage);
+   formData.message = areamessage;
  }
 
+ // print textarea from local memory
  function printTextarea(){
    const newMassage = localStorage.getItem(STORAGE_KEY);
    if(newMassage){
